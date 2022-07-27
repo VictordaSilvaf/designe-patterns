0010a0c9644147a1e09b2cf8b8ee8612f8167e84
VictordaSilvaf <victordasilvafernandes@gmail.com>
2022-07-27 17:28:32 -0300

    Final aula 5

--- public/index.php
+++ public/index.php
@@ -1,22 +1,32 @@
 <?php
 
-use Core\Creational\Builder\Conceptual\ApplePhone;
-use Core\Creational\Builder\Conceptual\SmartPhoneBuilder;
-use Core\Creational\Singleton\Practical\DbConnection;
+use Core\Creational\Builder\Conceptual\Request\BuilderRequests;
+use Core\Creational\Builder\Conceptual\Request\MethodsEnum;
 
 require_once '../vendor/autoload.php';
-
 // DbConnection::getConnection();
 
-$iphone12 = new SmartPhoneBuilder(
-    smartPhone: new ApplePhone(),
-    data: [
-        'gpu' => 'XPTO',
-        'cpu' => 'XPTO',
-        'ram' => 16,
-        'sensors' => ['XPTO'],
-        'model' => 'XPTO',
-    ]
-);
+// $iphone12 = new SmartPhoneBuilder(
+//     smartPhone: new ApplePhone(),
+//     data: [
+//         'gpu' => 'XPTO',
+//         'cpu' => 'XPTO',
+//         'ram' => 16,
+//         'sensors' => ['XPTO'],
+//         'model' => 'XPTO',
+//     ]
+// );
 
-$iphone12->getSmartPhone();
+// $iphone12->getSmartPhone();
+
+// $request = new BuilderRequests;
+
+// $request->url('viacep.com.br/ws/01001000/json/');
+// $request->method(MethodsEnum::GET);
+// $request->payload(['']);
+// $request->build();
+
+$request = (new BuilderRequests)
+    ->url('viacep.com.br/ws/01001000/json/')
+    ->method(MethodsEnum::GET)
+    ->build();
---
+++ src/Core/Conceptual/Builder/Conceitual/Request/BuilderRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Core\Creational\Builder\Conceptual\Request;
+
+class BuilderRequests implements RequestInterface
+{
+    protected $request;
+
+    public function __construct()
+    {
+        $this->request = new Request();
+    }
+
+    public function url(string $url): RequestInterface
+    {
+        $this->request->url = $url;
+        return $this;
+    }
+
+    public function payload(array $payload): RequestInterface
+    {
+        $this->request->payload = $payload;
+        return $this;
+    }
+
+    public function method(MethodsEnum $method): RequestInterface
+    {
+        $this->request->method = $method;
+        return $this;
+    }
+
+    public function build(): Request
+    {
+        return $this->request;
+    }
+}
